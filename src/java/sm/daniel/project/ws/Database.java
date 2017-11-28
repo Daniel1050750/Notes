8525f33bb04ed7f2b702bc7c836724ec90f06c48
Daniel Silva <1050750@gmail.com>
2017-11-28 22:11:26 +0000

    update 3

--- src/java/sm/daniel/project/ws/Database.java
+++ src/java/sm/daniel/project/ws/Database.java
@@ -1,5 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package sm.daniel.project.ws;
 
+/**
+ *
+ * @author Daniel
+ */
 public enum Database {
 	
 	USER("User"),
